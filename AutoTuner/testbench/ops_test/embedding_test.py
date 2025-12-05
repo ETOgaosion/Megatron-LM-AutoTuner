45ae5d44e9b34bdbfe76e69457705fc4acc58e0f
cyn1456492382 <1456492382@qq.com>
2025-12-05 16:56:33 +0800

    fix prepare_input in embedding_test

--- AutoTuner/testbench/ops_test/embedding_test.py
+++ AutoTuner/testbench/ops_test/embedding_test.py
@@ -88,14 +88,19 @@ class TestLanguageModelEmbedding(TestCommon):
 
     @override
     def prepare_input(self, test_case: InputTestCase, micro_batch: TensorDict):
-        micro_batch = micro_batch.to(torch.cuda.current_device())
-        micro_batch = micro_batch.contiguous()
-        input_ids_rmpad, attention_mask, position_ids_rmpad, packed_seq_params = (
-            get_thd_model_input_from_bshd(micro_batch)
-        )
-        self.seq_len = input_ids_rmpad.shape[1]
-        print("Seq len:", self.seq_len)
-        return input_ids_rmpad, position_ids_rmpad
+        if test_case.shape=='bshd':
+            micro_batch = micro_batch.to(torch.cuda.current_device())
+            micro_batch = micro_batch.contiguous()
+            input_ids_bshd = micro_batch.get("input_ids")
+            position_ids_bshd = micro_batch.get("position_ids")
+            return input_ids_bshd, position_ids_bshd
+        else:
+            micro_batch = micro_batch.to(torch.cuda.current_device())
+            micro_batch = micro_batch.contiguous()
+            input_ids_rmpad, attention_mask, position_ids_rmpad, packed_seq_params = (
+                get_thd_model_input_from_bshd(micro_batch)
+            )
+            return input_ids_rmpad, position_ids_rmpad
 
     @override
     def calculate_tokens(
@@ -113,6 +118,7 @@ class TestLanguageModelEmbedding(TestCommon):
         
         # Calculate activation memory
         if test_case.shape == 'bshd':
+            # activations: input_mask(dtype=bool)、masked_input(dtype=int64)
             activation_mem = test_case.micro_batch_size * test_case.seqlen * 8   \
                             + test_case.micro_batch_size * test_case.seqlen * 1
         else :  # thd
