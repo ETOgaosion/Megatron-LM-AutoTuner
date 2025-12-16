948d3762690786dfc1f57aaa4f534cf35377250a
gaoziyuan <gaoziyuan.955@bytedance.com>
2025-12-17 02:56:13 +0800

    try to run multinode

--- AutoTuner/testbench/functional/cpu_gpu_movements/interference_with_comm.py
+++ AutoTuner/testbench/functional/cpu_gpu_movements/interference_with_comm.py
@@ -35,7 +35,7 @@ args = get_args()
 def init_distributed(backend: str = "nccl"):
     """Initialize distributed environment."""
     os.environ['MASTER_ADDR'] = "10.124.44.156"
-    os.environ['MASTER_PORT'] = "29500"
+    os.environ['MASTER_PORT'] = "29600"
     os.environ['RANK'] = str(args.rank)
     rank = int(os.environ['RANK'])
     os.environ['LOCAL_WORLD_SIZE'] = "1"
